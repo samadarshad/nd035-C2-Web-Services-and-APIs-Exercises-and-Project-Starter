e408c43d29b10e84baffbb8937eeb6995d147bdb
samad <samadarshad@hotmail.co.uk>
2021-02-22 21:47:55 +0000

    dont need rest service

--- P02-VehiclesAPI/pricing-microservice/src/main/java/com/udacity/pricingmicroservice/repository/PriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udacity.pricingmicroservice.repository;
-
-import com.udacity.pricingmicroservice.entity.Price;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PriceRepository extends CrudRepository<Price, Long> {
-}
--- P02-VehiclesAPI/pricing-microservice/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO price (id, price, currency, vehicle_id) VALUES (1, 'price1','USD', 1);
-INSERT INTO price (id, price, currency, vehicle_id) VALUES (2, 'price2','USD', 2);
