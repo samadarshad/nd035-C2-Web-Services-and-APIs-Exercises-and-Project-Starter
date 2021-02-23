bb501412a65c232f334ae4f2688da0ca61fc51e2
samad <samadarshad@hotmail.co.uk>
2021-02-23 07:37:39 +0000

    working consuming eureka client

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,6 +22,7 @@ import org.springframework.web.reactive.function.client.WebClient;
  */
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
 public class VehiclesApiApplication {
 
     public static void main(String[] args) {
@@ -61,8 +66,13 @@ public class VehiclesApiApplication {
      * @return created pricing endpoint
      */
     @Bean(name="pricing")
-    public WebClient webClientPricing(@Value("${pricing.endpoint}") String endpoint) {
-        return WebClient.create(endpoint);
+    public WebClient webClientPricing(@Value("${pricing.endpoint}") String endpoint, LoadBalancerClient lbClient) {
+        return WebClient
+                .builder()
+                .filter(new LoadBalancerExchangeFilterFunction(lbClient))
+                .baseUrl(endpoint)
+                .build();
     }
+    //(new DefaultWebClientBuilder()).baseUrl(baseUrl).build();
 
 }
