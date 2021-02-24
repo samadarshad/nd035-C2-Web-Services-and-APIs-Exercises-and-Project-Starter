b564b0c2b3ab2114f3f225d9fd759437fdcb321f
samad <samadarshad@hotmail.co.uk>
2021-02-24 17:36:15 +0000

    ID is already generated

--- P02-VehiclesAPI/pricing-service-graphql/src/main/java/com/udacity/pricingservicegraphql/config/DemoData.java
+++ P02-VehiclesAPI/pricing-service-graphql/src/main/java/com/udacity/pricingservicegraphql/config/DemoData.java
@@ -11,10 +11,10 @@ public class DemoData {
     @Bean()
     CommandLineRunner initPriceDatabase(PriceRepository priceRepository) {
         return args -> {
-            priceRepository.save(new Price(1L, new BigDecimal(1500d), "USD", 1L));
-            priceRepository.save(new Price(2L, new BigDecimal(2000d), "USD", 2L));
-            priceRepository.save(new Price(3L, new BigDecimal(1000d), "USD", 3L));
-            priceRepository.save(new Price(4L, new BigDecimal(700d), "GBP", 3L));
+            priceRepository.save(new Price(null, new BigDecimal(1500d), "USD", 1L));
+            priceRepository.save(new Price(null, new BigDecimal(2000d), "USD", 2L));
+            priceRepository.save(new Price(null, new BigDecimal(1000d), "USD", 3L));
+            priceRepository.save(new Price(null, new BigDecimal(700d), "GBP", 3L));
         };
     }
 }
