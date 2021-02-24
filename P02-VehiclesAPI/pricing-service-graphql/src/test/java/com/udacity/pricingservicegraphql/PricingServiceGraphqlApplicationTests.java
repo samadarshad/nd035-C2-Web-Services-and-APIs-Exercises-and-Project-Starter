541e56a57e674503bfa329da7bfee933ab7cb8ee
samad <samadarshad@hotmail.co.uk>
2021-02-24 21:49:15 +0000

    working mutation

--- P02-VehiclesAPI/pricing-service-graphql/src/test/java/com/udacity/pricingservicegraphql/PricingServiceGraphqlApplicationTests.java
+++ P02-VehiclesAPI/pricing-service-graphql/src/test/java/com/udacity/pricingservicegraphql/PricingServiceGraphqlApplicationTests.java
@@ -19,7 +19,8 @@ import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.datasource.url=jdbc:h2:mem:PricingServiceGraphqlApplicationTests"})
 class PricingServiceGraphqlApplicationTests {
@@ -60,20 +61,25 @@ class PricingServiceGraphqlApplicationTests {
                 "}";
 
         String result = restTemplate.postForObject(url, request, String.class);
-        Assertions.assertEquals((double) JsonPath.read(result, "$.data.findPricesByVehicleId[0].price"), 1000.0);
-        Assertions.assertEquals((double) JsonPath.read(result, "$.data.findPricesByVehicleId[1].price"), 700.0);
+        assertEquals((double) JsonPath.read(result, "$.data.findPricesByVehicleId[0].price"), 1000.0);
+        assertEquals((double) JsonPath.read(result, "$.data.findPricesByVehicleId[1].price"), 700.0);
     }
 
     @Test
     void generateAndAssignPrice() {
         String request = "{\n" +
-                "    \"query\":\"{generateAndAssignPrice(currency: \"GBP\", vehicle_id: 3) { id price currency vehicle_id} }\"\n" +
+                "    \"query\":\"mutation{generateAndAssignPrice(currency: \\\"GBP\\\", vehicle_id: 3) { id price currency vehicle_id} }\"\n" +
                 "}";
 
-
+        String result = restTemplate.postForObject(url, request, String.class);
+        assertNotNull(JsonPath.read(result, "$.data.generateAndAssignPrice.price"));
+        assertEquals(JsonPath.read(result, "$.data.generateAndAssignPrice.vehicle_id"), (Integer) 3);
+        assertEquals(JsonPath.read(result, "$.data.generateAndAssignPrice.currency"), "GBP");
     }
 
 }
+
+
 
 //    generateAndAssignPrice(currency: String!, vehicle_id: Int!): Price!
 //        newPrice(price: Float!, currency: String!, vehicle_id: Int!) : Price!
