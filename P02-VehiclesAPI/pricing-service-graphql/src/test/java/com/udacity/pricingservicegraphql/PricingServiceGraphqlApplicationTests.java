5ace0a90f3d7de3f962c81ae9f34155a6ff10536
samad <samadarshad@hotmail.co.uk>
2021-02-24 21:08:35 +0000

    working setup and test

--- P02-VehiclesAPI/pricing-service-graphql/src/test/java/com/udacity/pricingservicegraphql/PricingServiceGraphqlApplicationTests.java
+++ P02-VehiclesAPI/pricing-service-graphql/src/test/java/com/udacity/pricingservicegraphql/PricingServiceGraphqlApplicationTests.java
@@ -2,8 +2,10 @@ package com.udacity.pricingservicegraphql;
 
 
 import com.jayway.jsonpath.JsonPath;
+import com.udacity.pricingservicegraphql.entity.Price;
 import com.udacity.pricingservicegraphql.repository.PriceRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,30 +13,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.datasource.url=jdbc:h2:mem:PricingServiceGraphqlApplicationTests"})
 @AutoConfigureMockMvc
 class PricingServiceGraphqlApplicationTests {
 
     @LocalServerPort
     private int port;
 
-    @Autowired
-    private PriceRepository priceRepository;
-
-//    @Before
-//    public void setUp() {
-//        priceRepository.save(new Price(null, new BigDecimal(1500d), "USD", 1L));
-//        priceRepository.save(new Price(null, new BigDecimal(2000d), "USD", 2L));
-//        priceRepository.save(new Price(null, new BigDecimal(1000d), "USD", 3L));
-//        priceRepository.save(new Price(null, new BigDecimal(700d), "GBP", 3L));
-//        priceRepository.save(new Price(null, new BigDecimal(700d), "GBP", 3L));
-//        priceRepository.save(new Price(null, new BigDecimal(700d), "GBP", 3L));
-//    }
-
+    @BeforeAll
+    static void beforeAll(@Autowired PriceRepository priceRepository) {
+        priceRepository.save(new Price(null, new BigDecimal(1500d), "USD", 1L));
+        priceRepository.save(new Price(null, new BigDecimal(2000d), "USD", 2L));
+        priceRepository.save(new Price(null, new BigDecimal(1000d), "USD", 3L));
+        priceRepository.save(new Price(null, new BigDecimal(700d), "GBP", 3L));
+    }
     @Test
-    void listPrices() throws Exception {
+    void listPrices() {
         String expectedResponse = "{\"data\":{\"findAllPrices\":[{\"id\":\"1\",\"price\":1500.0,\"currency\":\"USD\",\"vehicle_id\":1},{\"id\":\"2\",\"price\":2000.0,\"currency\":\"USD\",\"vehicle_id\":2},{\"id\":\"3\",\"price\":1000.0,\"currency\":\"USD\",\"vehicle_id\":3},{\"id\":\"4\",\"price\":700.0,\"currency\":\"GBP\",\"vehicle_id\":3}]}}";
         String request = "{\n" +
                 "    \"query\":\"{findAllPrices { id price currency vehicle_id} }\"\n" +
