c28304fd543d6a260be0eee06825d7e88170ca5d
samad <samadarshad@hotmail.co.uk>
2021-02-24 15:07:27 +0000

    adding GraphQl pricing service

---
+++ P02-VehiclesAPI/pricing-service-graphql/src/main/java/com/udacity/pricingservicegraphql/PricingServiceGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.pricingservicegraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingServiceGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingServiceGraphqlApplication.class, args);
+    }
+
+}
